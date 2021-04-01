4b4d9b38ed30a6137d2d6e10290f7a3f08343657
Marcos Mateus <62218308+MarcosMateusOS@users.noreply.github.com>
2021-04-01 18:46:56 -0300

    Cadastro feito pelo front persistindo no back check

--- front-end-Web/web/src/components/RegisterCard/index.js
+++ front-end-Web/web/src/components/RegisterCard/index.js
@@ -1,22 +1,79 @@
-import React from 'react';
+import React,{ useState, useEffect }from 'react';
 import  * as S from './styles'
 
 
 
 function RegisterCard() {
+
+  const[name,setName] = useState();
+  const[email,setEmail] = useState();
+  const[firstPassword,setFirst] = useState();
+  const[secondPassword,setSecond] = useState();
+  const[date,setDate] = useState();
+
+  function validatePassword(){
+
+    if(firstPassword == secondPassword)
+    {
+      return true;
+    }else
+      return false;
+  }
+
+  function register()
+  {
+    if(validatePassword())
+    {
+      alert('CADASTRO FEITO COM SUCESSo')
+    }else
+    alert('SENHA DIFERENTES')
+
+  }
   return (
 
     <S.Container>
 
       
-      <input type="text" placeholder="Nome"/>
-      <input type="text" placeholder="E-mail"/>
-      <input type="date" placeholder="Data de Nascimento"/>
-      <input type="password" placeholder="Senha"/>
-      <input type="password" placeholder="Confirmar Senha"/>
+      <input 
+            type="text" 
+            placeholder="Nome"
+            onChange={e => setName(e.target.value)}
+            value={name}   
+      />
+      <input 
+            type="text" 
+            placeholder="E-mail"
+            onChange={e => setEmail(e.target.value)}
+            value={email} 
+      />
+      <input 
+            type="date" 
+            placeholder="Data de Nascimento"
+            onChange={e => setDate(e.target.value)}
+            value={date} 
+      />
+      <input 
+            type="password" 
+            placeholder="Senha"
+            onChange={e => setFirst(e.target.value)}
+            value={firstPassword} 
+
+      />
+      <input 
+            type="password" 
+            placeholder="Confirmar Senha"
+            onChange={e => setSecond(e.target.value)}
+            value={secondPassword}             
+      />
 
       <S.buttonsArea>
-        <button type='button'>CADASTRAR</button>
+        <button 
+                type='button' 
+                onClick={()=> register()}
+        >
+          CADASTRAR
+          
+        </button>
       </S.buttonsArea>
         
     </S.Container>
--- front-end-Web/web/src/views/Task/index.js
+++ front-end-Web/web/src/views/Task/index.js
@@ -8,16 +8,18 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 
-import iconCalendar from '../../assets/calendar.png';
-import iconClock from '../../assets/clock.png'
-
-
-
 function Task() {
 
  
   const[lateCount,setLateCount]=useState();
   const[type,setType]=useState();
+  const[id, setId]=useState();
+  const[macaddress,setMacaddress] = useState('00:D0:56:F2:B5:12');
+  const[done,setDone] = useState(false);
+  const[title,setTitle] = useState();
+  const[description,setDescription] = useState();
+  const[date,setDate] = useState();
+  const[hour,setHour] = useState();
 
   
 
@@ -27,6 +29,20 @@ function Task() {
     .then(response => {
       setLateCount(response.data.length)
     })
+
+  }
+
+  async function save(){
+
+    await api.post('/task/registerTask',{
+      macaddress,
+      type,
+      title,
+      description,
+      when: `${date}T${hour}:00.000`
+    }).then(() =>
+      alert('TAREFA CADASTRADA COM SUCESSO')
+    )
 
   }
 
@@ -59,30 +75,57 @@ function Task() {
            
         <S.Input>
             <span>Título</span>
-            <input type="text" placeholder="Título da Tarefa"></input>
+            <input 
+                  type="text" 
+                  placeholder="Título da Tarefa" 
+                  onChange={e => setTitle(e.target.value)}
+                  value={title}      
+            />
         </S.Input>
 
         <S.TextArea>
             <span>Detalhes</span>
-            <textarea rows={5} placeholder="Detalhes da Tarefa"/>
+            <textarea 
+                      rows={5} 
+                      placeholder="Detalhes da Tarefa..."
+                      onChange={e => setDescription(e.target.value)}
+                      value={description}   
+                      
+            />
         </S.TextArea>
 
         <S.Input>
             <span>Data</span>
-            <input type="date" placeholder="Título da Tarefa"></input>
+            <input 
+                  type="date" 
+                  placeholder="Título da Tarefa"
+                  onChange={e => setDate(e.target.value)}
+                  value={date}   
+
+            />
             
         </S.Input>
 
         <S.Input>
             <span>Hora</span>
-            <input type="time" placeholder="Título da Tarefa"></input>
+            <input 
+                  type="time" 
+                  placeholder="Título da Tarefa"
+                  onChange={e => setHour(e.target.value)}
+                  value={hour}   
+            
+            />
            
         </S.Input>
 
         <S.Options>
 
             <div>
-              <input type="checkbox"/>
+              <input 
+                    type="checkbox"
+                    check={done}
+                    onChange={()=>setDone(!done)}
+              />
               <span>CONCLUÍDO</span>
             </div>
 
@@ -94,7 +137,7 @@ function Task() {
       </S.Form>
 
       <S.Save>
-            <button type="button">SALVAR </button>
+            <button type="button" onClick={save}>SALVAR </button>
       </S.Save>
 
 
